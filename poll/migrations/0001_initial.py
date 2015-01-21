478016e0fe6d821a642db9708d8605045efc08e1
tomasz.roszko <tomaszroszko@gmail.com>
2015-01-21 15:23:00 +0100

    Make it work with django 1.7.x

--- poll/migrations/0001_initial.py
+++ poll/migrations/0001_initial.py
@@ -1,115 +1,67 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
-
-class Migration(SchemaMigration):
-
-    def forwards(self, orm):
-        
-        # Adding model 'Poll'
-        db.create_table('poll_poll', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=250)),
-            ('is_published', self.gf('django.db.models.fields.BooleanField')(default=True, blank=True)),
-            ('date', self.gf('django.db.models.fields.DateField')(default=datetime.date.today)),
-        ))
-        db.send_create_signal('poll', ['Poll'])
-
-        # Adding model 'Item'
-        db.create_table('poll_item', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('poll', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['poll.Poll'])),
-            ('value', self.gf('django.db.models.fields.CharField')(max_length=250)),
-            ('pos', self.gf('django.db.models.fields.SmallIntegerField')(default='0')),
-        ))
-        db.send_create_signal('poll', ['Item'])
-
-        # Adding model 'Vote'
-        db.create_table('poll_vote', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('poll', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['poll.Poll'])),
-            ('item', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['poll.Item'])),
-            ('ip', self.gf('django.db.models.fields.IPAddressField')(max_length=15)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, blank=True)),
-            ('datetime', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-        ))
-        db.send_create_signal('poll', ['Vote'])
+from django.conf import settings
 
 
-    def backwards(self, orm):
-        
-        # Deleting model 'Poll'
-        db.delete_table('poll_poll')
+class Migration(migrations.Migration):
 
-        # Deleting model 'Item'
-        db.delete_table('poll_item')
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-        # Deleting model 'Vote'
-        db.delete_table('poll_vote')
-
-
-    models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        'auth.permission': {
-            'Meta': {'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'poll.item': {
-            'Meta': {'object_name': 'Item'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'poll': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['poll.Poll']"}),
-            'pos': ('django.db.models.fields.SmallIntegerField', [], {'default': "'0'"}),
-            'value': ('django.db.models.fields.CharField', [], {'max_length': '250'})
-        },
-        'poll.poll': {
-            'Meta': {'object_name': 'Poll'},
-            'date': ('django.db.models.fields.DateField', [], {'default': 'datetime.date.today'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_published': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '250'})
-        },
-        'poll.vote': {
-            'Meta': {'object_name': 'Vote'},
-            'datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'ip': ('django.db.models.fields.IPAddressField', [], {'max_length': '15'}),
-            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['poll.Item']"}),
-            'poll': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['poll.Poll']"}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['poll']
+    operations = [
+        migrations.CreateModel(
+            name='Item',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('value', models.CharField(max_length=250, verbose_name=b'value')),
+                ('pos', models.SmallIntegerField(default=b'0', verbose_name=b'position')),
+            ],
+            options={
+                'ordering': ['pos'],
+                'verbose_name': 'answer',
+                'verbose_name_plural': 'answers',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Poll',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=250, verbose_name=b'question')),
+                ('date', models.DateField(default=datetime.date.today, verbose_name=b'date')),
+                ('is_published', models.BooleanField(default=True, verbose_name=b'is published')),
+            ],
+            options={
+                'ordering': ['-date'],
+                'verbose_name': 'poll',
+                'verbose_name_plural': 'polls',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Vote',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('ip', models.IPAddressField(verbose_name=b"user's IP")),
+                ('datetime', models.DateTimeField(auto_now_add=True)),
+                ('item', models.ForeignKey(verbose_name=b'voted item', to='poll.Item')),
+                ('poll', models.ForeignKey(verbose_name=b'poll', to='poll.Poll')),
+                ('user', models.ForeignKey(verbose_name=b'user', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'verbose_name': 'vote',
+                'verbose_name_plural': 'votes',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='poll',
+            field=models.ForeignKey(to='poll.Poll'),
+            preserve_default=True,
+        ),
+    ]
--- poll/models.py
+++ poll/models.py
@@ -1,22 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import datetime
-from django.db import models
-from django.utils.translation import gettext as _
-from django.db.models.manager import Manager
-from django.core.exceptions import ValidationError
 
-try:
-    from django.contrib.auth import get_user_model
-except ImportError:
-    from django.contrib.auth.models import User
-else:
-    User = get_user_model()
+from django.conf import settings
+from django.db import models
+from django.db.models.manager import Manager
+from django.utils.translation import gettext as _
 
 
 class PublishedManager(Manager):
     def get_query_set(self):
         return super(PublishedManager, self).get_query_set().filter(is_published=True)
+
 
 class Poll(models.Model):
     title = models.CharField(max_length=250, verbose_name=_('question'))
@@ -64,7 +59,7 @@ class Vote(models.Model):
     poll = models.ForeignKey(Poll, verbose_name=_('poll'))
     item = models.ForeignKey(Item, verbose_name=_('voted item'))
     ip = models.IPAddressField(verbose_name=_('user\'s IP'))
-    user = models.ForeignKey(User, blank=True, null=True,
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True,
                              verbose_name=_('user'))
     datetime = models.DateTimeField(auto_now_add=True)
 
@@ -73,7 +68,6 @@ class Vote(models.Model):
         verbose_name_plural = _('votes')
 
     def __unicode__(self):
-        if isinstance(self.user, User):
-            username_field = getattr(User, 'USERNAME_FIELD', 'username')
-            return getattr(User, username_field, '')
+        if self.user:
+            unicode(self.user)
         return self.ip
