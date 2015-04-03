414c7ef2f90b05a4e0d09948f7e9e5661e525f97
tomasz.roszko <tomaszroszko@gmail.com>
2015-04-03 10:22:05 +0200

    Display percentage for each answer and votes number

--- poll/admin.py
+++ poll/admin.py
@@ -1,15 +1,29 @@
 from django.contrib import admin
-from poll.models import *
+from poll.models import Item, Poll
+from poll.templatetags.poll_tags import percentage
 from django.utils.translation import gettext as _
+
 
 class PollItemInline(admin.TabularInline):
     model = Item
     extra = 0
     max_num = 15
+    fields = ('value', 'pos', 'percentage', 'votes')
+    readonly_fields = ('percentage', 'votes')
+
+    def percentage(self, obj):
+        "display percentage result for each answer"
+        return percentage(obj.poll, obj)
+
+    def votes(self, obj):
+        "return votes for answer"
+        return obj.get_vote_count()    
+
 
 class PollAdmin(admin.ModelAdmin):
     list_display = ('title', 'date', 'vote_count', 'is_published')
-    inlines = [PollItemInline,]
+    inlines = [PollItemInline]
+
 
 admin.site.register(Poll, PollAdmin)
 
@@ -19,3 +33,4 @@ admin.site.register(Poll, PollAdmin)
 #     list_filter = ('poll', 'datetime')
 # 
 # admin.site.register(Vote, VoteAdmin)
+
